a00a34a8aa7606c789f208a6c8733efdd7422a7a
Joe van Bohemen <joseph.vanbohemen@milkbooks.com>
2022-12-06 16:09:19 +0000

    camel

--- src/day-six/dataParser.ts
+++ src/day-six/dataParser.ts
@@ -1,23 +1,23 @@
 export default class DataParser {
   startMarker: string
   numCharsProcessed: number
-  constructor(private packetStr: string, public markerlength: number) {
+  constructor(private packetStr: string, public markerLength: number) {
     for (let i = 0; i < this.packetStr.length; i++) {
-      const marker = this.packetStr.substring(i, i + this.markerlength)
+      const marker = this.packetStr.substring(i, i + this.markerLength)
       const unique: {
         [char: string]: boolean
       } = {}
       let uniqueCharCount = 0
-      for (let m = 0; m < this.markerlength; m++) {
+      for (let m = 0; m < this.markerLength; m++) {
         const c = marker[m]
         if (!unique[c]) {
           uniqueCharCount++
           unique[c] = true
         }
       }
-      if (uniqueCharCount == this.markerlength) {
+      if (uniqueCharCount == this.markerLength) {
         this.startMarker = marker
-        this.numCharsProcessed = i + this.markerlength
+        this.numCharsProcessed = i + this.markerLength
         break
       }
     }
