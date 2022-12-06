bb56459b79781c2de01ad06ecb423d9b6b3add28
Joe van Bohemen <joseph.vanbohemen@milkbooks.com>
2022-12-06 16:25:47 +0000

    move shared method to base class

--- src/day-five/crane.ts
+++ src/day-five/crane.ts
@@ -3,13 +3,6 @@ import Instruction from './instruction';
 abstract class Crane {
   constructor(public crateStacks: string[][]) {}
   abstract executeInstruction(i: Instruction): void
-  abstract get topCrates(): string
-}
-export class Crane_9001 extends Crane {
-  executeInstruction(i: Instruction) {
-    const toMove = this.crateStacks[i.from].splice(i.quantity * -1, i.quantity)
-    this.crateStacks[i.to].push(...toMove)
-  }
   get topCrates(): string {
     let topCrateStr = ''
     for (const stack of this.crateStacks) {
@@ -17,6 +10,12 @@ export class Crane_9001 extends Crane {
       topCrateStr += topCrateChar
     }
     return topCrateStr
+  }
+}
+export class Crane_9001 extends Crane {
+  executeInstruction(i: Instruction) {
+    const toMove = this.crateStacks[i.from].splice(i.quantity * -1, i.quantity)
+    this.crateStacks[i.to].push(...toMove)
   }
 }
 
@@ -30,13 +29,5 @@ export class Crane_9000 extends Crane {
         console.error('tried to move crate from empty stack', i.from)
       }
     }
-  }
-  get topCrates(): string {
-    let topCrateStr = ''
-    for (const stack of this.crateStacks) {
-      const topCrateChar = stack[stack.length - 1]
-      topCrateStr += topCrateChar
-    }
-    return topCrateStr
   }
 }
