d8d742122d24d1db4e2b19ca3526101264380a15
Joe van Bohemen <joseph.vanbohemen@milkbooks.com>
2022-12-08 23:46:50 +0000

    seven:1 done, complete refactor

--- src/day-seven/index.ts
+++ src/day-seven/index.ts
@@ -1,52 +1,93 @@
 import fs from 'fs'
-import { DirEnt, Dir, File } from './dirEnt'
 import ShellParser from './parser'
+
+const MAX_SPACE = 70000000
+const REQUIRED_SPACE = 30000000
 
 export default () => {
   const parser = new ShellParser(__dirname + '/data.txt')
-  let current: Dir = null
-  let rootDir: Dir = null
+  let activeDirs: string[] = []
+  const dirSizes: {
+    [dirName: string]: number
+  } = {}
+  const added: {
+    [path: string]: boolean
+  } = {}
   for (const command of parser.pastCommands) {
     switch (command.cmd) {
       case 'cd':
         const dirName = command.args[0]
         if (dirName == '..') {
-          if (current) {
-            current = current.parent
-          }
+          activeDirs.pop()
         }
         else {
-          // handle rerun cd
-          let child: Dir = current && current.children.find((c: Dir) => c.isDir && c.name == dirName)
-          if (!child) {
-            child = new Dir(dirName)
-            child.parent = current
-          }
-          current = child
-          if (current.name == '/') {
-            rootDir = current
+          if (dirName == '/') {
+            activeDirs = ['/']
+          } else {
+            let lastActive = activeDirs[activeDirs.length - 1]
+            if (!lastActive.endsWith('/')) {
+              lastActive += '/'
+            }
+            activeDirs.push(lastActive + dirName)
           }
         }
         break
       case 'ls':
-        // handle rerun ls
-        if (current?.children.length == 0) {
-          for (const line of command.output) {
-            const [pref, name] = line.split(' ')
-            let child: DirEnt = null
-            if (pref == 'dir') {
-              child = new Dir(name)
-            } else {
-              child = new File(name, pref)
+        for (const line of command.output) {
+          const [pref, name] = line.split(' ')
+          if (pref == 'dir') {
+            continue
+          }
+          const size = Number(pref)
+          for (let i = 0; i < activeDirs.length; i++) {
+            const dirName = activeDirs[i]
+            const path = [...activeDirs.slice(0, i + 1), name].join('/').substring(1)
+            if (!added[path]) {
+              if (!dirSizes[dirName]) {
+                dirSizes[dirName] = 0
+              }
+              dirSizes[dirName] += size
             }
-            child.parent = current
-            current.children.push(child)
+            added[path] = true
           }
         }
         break
     }
   }
-  console.log(rootDir.size)
+  // fs.writeFileSync(__dirname + '/../../test.json', JSON.stringify(dirSizes, null, 2))
+  // let total = 0
+  // for (const dirName in dirSizes) {
+    // if (dirSizes[dirName] <= 100000) {
+      // total += dirSizes[dirName]
+    // }
+  // }
+  // console.log('total', total) // 1989474 is too low
+  const usedSpace = MAX_SPACE - dirSizes['/']
+  const toDelete = usedSpace - REQUIRED_SPACE
+  let closestDir: {
+    name: string
+    size: number
+  } = null
+  for (const n in dirSizes) {
+    const s = dirSizes[n]
+    if (!closestDir) {
+      closestDir = {
+        name: n,
+        size: s
+      }
+      continue
+    }
+    if (s < toDelete) {
+      continue
+    }
+    if (s < closestDir.size) {
+      closestDir = {
+        name: n,
+        size: s
+      }
+    }
+  }
+  console.log('smallest to delete', closestDir)
 }
 
 
